e260dff6b0edda3545b64d51fc8f7aa24ae5976e
MeijerM1 <max1_meijer@hotmail.com>
2019-03-25 18:13:04 +0100

    Add tweet posting

---
+++ src/app/components/tweet/tweet.component.ts
@@ -0,0 +1,36 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {Tweet} from "../../domain/Tweet";
+import {TweetService} from "../../services/tweet/tweet.service";
+import {AuthenticatorService} from "../../services/authentication/authenticator.service";
+import {MAT_DIALOG_DATA, MatDialogRef, MatSnackBar} from "@angular/material";
+
+@Component({
+  selector: 'app-tweet',
+  templateUrl: './tweet.component.html',
+  styleUrls: ['./tweet.component.sass']
+})
+export class TweetComponent  {
+
+  tweet: Tweet = new Tweet();
+
+  constructor(public dialogRef: MatDialogRef<TweetComponent>,
+              private tweetService: TweetService,
+              private authService: AuthenticatorService,
+              private snackbar: MatSnackBar) {
+    this.tweet.authorUuid = this.authService.currentUser.uuid;
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  postTweet() {
+    console.log(this.tweet);
+    this.tweetService.postTweet(this.tweet).subscribe(() => {
+      this.snackbar.open('Tweet posted!', 'dismiss', {
+        duration: 2000
+      });
+      this.dialogRef.close();
+    });
+  }
+}
